180f6860015645af126f9e2847262d06af302e8d
olligobber <sirolligobbervii@yahoo.com.au>
2018-02-26 18:47:15 +1100

    Fixed bug involving change of depth during simplification, and improved error messages

--- ProcessLambda.hs
+++ ProcessLambda.hs
@@ -13,6 +13,7 @@ data Expression =
     Bound Integer |
     Apply Expression Expression |
     Unbound String
+--    deriving Show
 
 -- Default variable names
 variables :: [String]
@@ -35,12 +36,25 @@ unboundVariables (Apply x y) =
     Set.union (unboundVariables x) (unboundVariables y)
 unboundVariables (Unbound s) = Set.singleton s
 
+{-
+increaseDepth k n x replaces all binds in x to a level more than n
+with a bind k levels below that
+-}
+increaseDepth :: Integer -> Integer -> Expression -> Expression
+increaseDepth k n (Lambda x) = Lambda (increaseDepth k (n+1) x)
+increaseDepth k n (Bound m)
+    | m > n     = Bound (m+k)
+    | otherwise = Bound m
+increaseDepth k n (Apply x y) =
+    Apply (increaseDepth k n x) (increaseDepth k n y)
+increaseDepth _ _ (Unbound s) = Unbound s
+
 -- Recursively unbinds variables bound to the lambda n levels below
 -- First variable is the expression to replace with
 unbindAt :: Expression -> Integer -> Expression -> Expression
 unbindAt t n (Lambda x) = Lambda (unbindAt t (n+1) x)
 unbindAt t m (Bound n)
-    | m == n    = t
+    | m == n    = increaseDepth (n-1) 0 t
     | otherwise = Bound n
 unbindAt t n (Apply x y) = Apply (unbindAt t n x) (unbindAt t n y)
 unbindAt _ _ (Unbound s) = Unbound s
@@ -50,8 +64,8 @@ unbindAt _ _ (Unbound s) = Unbound s
 showExpression' :: Expression -> [String] -> String
 showExpression' (Lambda x) (v:vs) =
     "λ" ++ v ++ "." ++ showExpression' (unbindAt (Unbound v) 1 x) vs
-showExpression' (Lambda _) _ = undefined -- Ran out of variable names
-showExpression' (Bound _) _ = undefined -- Malformed expression
+showExpression' (Lambda _) _ = error "Error: Run out of variable names"
+showExpression' (Bound _) _ = error "Error: Malformed expression"
 showExpression' (Apply (Lambda x) y) v =
     "(" ++ showExpression' (Lambda x) v ++ ") " ++ showExpression' y v
 showExpression' (Apply x y) v =
@@ -70,10 +84,10 @@ simplify (Lambda x) = Lambda <$> simplify x
 simplify (Bound _) = Nothing
 simplify (Unbound _) = Nothing
 simplify (Apply (Lambda x) y) = case (simplify x, simplify y) of
-    (Just a, Just b) -> Just (unbindAt b 1 a)
-    (Just a, Nothing) -> Just (unbindAt y 1 a)
-    (Nothing, Just b) -> Just (unbindAt b 1 x)
-    (Nothing, Nothing) -> Just (unbindAt y 1 x)
+        (Just a, Just b) -> Just (unbindAt b 1 a)
+        (Just a, Nothing) -> Just (unbindAt y 1 a)
+        (Nothing, Just b) -> Just (unbindAt b 1 x)
+        (Nothing, Nothing) -> Just (unbindAt y 1 x)
 simplify (Apply x y) = case (simplify x, simplify y) of
     (Just a, Just b) -> Just (Apply a b)
     (Just a, Nothing) -> Just (Apply a y)
